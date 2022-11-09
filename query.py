bfffa7293c9b4d109604ac9911ca305b60d36d64
Pizza623 <64260520+Pizza623@users.noreply.github.com>
2022-11-09 18:46:06 +0800

    joinp2

--- query.py
+++ query.py
@@ -3,18 +3,18 @@ from utils.plan import get_qep_nodes, get_query_plan
 if __name__ == '__main__':
 
     disable = tuple(["hashjoin"])
-    list_scan, list_join = get_qep_nodes(5, disable)
+    list_scan, list_join = get_qep_nodes(19, disable)
 
 
 
     for node in list_scan:
-        # print(node.__repr__())
+        print(node.__repr__())
         print(node.node_type, node.relation_name)
 
     print()
     print()
     for node in list_join:
-        # print(node)
+        print(node)
         # print(node.node_type)
         if node.node_type == "HASH JOIN":
             print(node.node_type, node.hash_condition)
@@ -24,14 +24,14 @@ if __name__ == '__main__':
                 print(node.node_type, node.join_filter)
         else: print(node.node_type)
 
-    splitted_query = get_query_plan(5, disable, True)
+    splitted_query = get_query_plan(19, disable, True)
     print()
     for each_row in splitted_query:
-        if ("WHERE" in each_row) or ("AND" in each_row):
+        if ("WHERE" in each_row) or ("AND" in each_row) or ("OR" in each_row):
             each_word = each_row.split(" ")
             if ("WHERE" in each_word):
                 del each_word[0:1]
-            elif ("AND" in each_word):
+            elif ("AND" in each_word) or ("OR" in each_row):
                 del each_word[0:3]
 
             for node in list_join:
